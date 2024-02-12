30a2dfe490568ec67a88f96f6904269738e614fd
Andy Taylor <andy.taylor@mail.com>
2024-02-12 11:31:40 +0000

    Modified the data resoruce for compute to reflect Amazon Linux 2003

--- terraform/modules/compute/main.tf
+++ terraform/modules/compute/main.tf
@@ -5,21 +5,18 @@
 
 # Data resource to determine the latest Amazon Linux2 AMI
 data "aws_ami" "amazon_linux" {
-  most_recent = true
-  owners      = ["amazon"]
+most_recent = true
 
-  filter {
-    name = "name"
-    values = [
-      "amzn-ami-hvm-*-x86_64-gp2",
-    ]
+owners      = ["amazon"]
+
+filter {
+name   = "name"
+values = ["al2023-ami-2023.3.20240131.0-kernel-6.1-x86_64"]
   }
 
-  filter {
-    name = "owner-alias"
-    values = [
-      "amazon",
-    ]
+filter {
+name   = "virtualization-type"
+values = ["hvm"]
   }
 }
 
