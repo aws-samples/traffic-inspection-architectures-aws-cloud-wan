166615d14397721614b55bff609fff6e46902c7a
Pablo Sánchez Carmona <pablosc@amazon.es>
2024-08-21 11:28:45 +0200

    minor updates

--- terraform/centralized_outbound_region_without_inspection/outputs.tf
+++ terraform/centralized_outbound_region_without_inspection/outputs.tf
@@ -3,33 +3,29 @@
 
 # --- centralized_outbound_region_without_inspection/outputs.tf ---
 
-output "policy" {
-  value = data.aws_networkmanager_core_network_policy_document.policy.json
+output "cloud_wan" {
+  description = "AWS Cloud WAN resources."
+  value = {
+    global_network = aws_networkmanager_global_network.global_network.id
+    core_network   = aws_networkmanager_core_network.core_network.id
+  }
 }
 
-# output "cloud_wan" {
-#   description = "AWS Cloud WAN resources."
-#   value = {
-#     global_network = aws_networkmanager_global_network.global_network.id
-#     core_network   = aws_networkmanager_core_network.core_network.id
-#   }
-# }
-
-# output "vpcs" {
-#   description = "VPCs created."
-#   value = {
-#     ireland = {
-#       spokes     = { for k, v in module.ireland_spoke_vpcs : k => v.vpc_attributes.id }
-#       inspection = module.ireland_inspection_vpc.central_vpcs.inspection.vpc_attributes.id
-#     }
-#     nvirginia = {
-#       spokes     = { for k, v in module.nvirginia_spoke_vpcs : k => v.vpc_attributes.id }
-#       inspection = module.nvirginia_inspection_vpc.central_vpcs.inspection.vpc_attributes.id
-#     }
-#     sydney = {
-#       spokes     = { for k, v in module.sydney_spoke_vpcs : k => v.vpc_attributes.id }
-#       inspection = module.sydney_inspection_vpc.central_vpcs.inspection.vpc_attributes.id
-#     }
-#     london_spokes = { for k, v in module.london_spoke_vpcs : k => v.vpc_attributes.id }
-#   }
-# }
+output "vpcs" {
+  description = "VPCs created."
+  value = {
+    ireland = {
+      spokes     = { for k, v in module.ireland_spoke_vpcs : k => v.vpc_attributes.id }
+      inspection = module.ireland_inspection_vpc.central_vpcs.inspection.vpc_attributes.id
+    }
+    nvirginia = {
+      spokes     = { for k, v in module.nvirginia_spoke_vpcs : k => v.vpc_attributes.id }
+      inspection = module.nvirginia_inspection_vpc.central_vpcs.inspection.vpc_attributes.id
+    }
+    sydney = {
+      spokes     = { for k, v in module.sydney_spoke_vpcs : k => v.vpc_attributes.id }
+      inspection = module.sydney_inspection_vpc.central_vpcs.inspection.vpc_attributes.id
+    }
+    london_spokes = { for k, v in module.london_spoke_vpcs : k => v.vpc_attributes.id }
+  }
+}
